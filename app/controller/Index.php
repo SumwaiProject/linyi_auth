cd073e8a232889284e4da0818298951fe1a94ced
sumwai <sumwai@qq.com>
2022-09-16 11:51:33 +0000

    create email test

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -2,6 +2,7 @@
 namespace app\controller;
 
 use app\BaseController;
+use Mail;
 
 class Index extends BaseController
 {
@@ -11,6 +12,15 @@ class Index extends BaseController
     public function index()
     {
         return "Hello, This is `auth_linyi_new` project";
+    }
+
+    /**
+     * test send email
+     *
+     * @return void
+     */
+    public function sendmail(){
+        // return Mail::send('admin@lim1.cn', "Hello", '<a href="https://sumwai.cn/">查看详情</a>') ? 'send mail success' : 'failed to send mail';
     }
 
 }
---
+++ extend/Mail.php
@@ -0,0 +1,56 @@
+<?php
+
+use \PHPMailer\PHPMailer\PHPMailer;
+use \PHPMailer\PHPMailer\Exception;
+
+class Mail
+{
+
+    /**
+     * mail -> send
+     *
+     * @param  string $to      to somebody
+     * @param  string $subject mail title
+     * @param  string $body    mail content
+     * @return bool
+     */
+    public static function send(string $to = '', string $subject = '', string $body = '')
+    {
+        $mail = new PHPMailer(true);
+    
+        try {
+            //Server settings
+            // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = env('smtp.server', 'smtp.qq.com');                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = env('smtp.user', 'sumwai@qq.com');                     //SMTP username
+            $mail->Password   = env('smtp.password', '');                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = env('smtp.port', 465);                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom(env('smtp.user'), env('smtp.name'));
+            $mail->addAddress($to);     //Add a recipient
+            // $mail->addReplyTo('info@example.com', 'Information');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
+
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = $subject;
+            $mail->Body    = $body;
+            // $mail->AltBody = $body;
+
+            $mail->send();
+        } catch (Exception $e) {
+            return false;
+            // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+        return true;
+    }
+}
